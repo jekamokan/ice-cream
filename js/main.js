9c79dbf986b3d48712d2544fab9b71057cacf362
jekamokan <jekamokan1994@gmail.com>
2022-10-22 19:24:17 +0200

    modal-2

--- js/main.js
+++ js/main.js
@@ -19,4 +19,26 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 2500,
   });
+      
+      $('.header__btn').click(function (event) {
+      $('.popupone').addClass('popupone__active');
+      })
+      $('.header__btn').click(function (event) {
+      $('.popupone__buy-body').addClass('popupone__animation');
+      })
+      $('.popupone__buy-cross').click(function (event) {
+      $('.popupone').removeClass('popupone__active');
+      })
+      
+      
+      $('.products__item-link').click(function (event) {
+      $('.popuptwo').addClass('popuptwo__active');
+      })
+      $('.popuptwo__cross').click(function (event) {
+      $('.popuptwo').removeClass('popuptwo__active');
+       })
 });
+
+
+
+products__item-link
